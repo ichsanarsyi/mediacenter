b5a095e0ec69015209f6db290395b524ff97cf0c
Ichsan Arsyi Putra <49037429+ichsanarsyi@users.noreply.github.com>
2026-04-15 07:35:04 +0700

    start fix ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,20 +7,24 @@
 	class="fixed z-99 flex h-24 w-full items-center justify-center rounded-b-2xl bg-linear-to-br
  from-blue-500 to-cyan-500 py-2 text-center text-2xl font-extrabold text-white"
 >
-	<h1>Media Center by Diskominfo Kab Semarang</h1>
+	<h1 class="flex flex-col items-center md:flex-row">
+		Media Center <span class="mx-2 text-sm font-normal md:text-2xl md:font-extrabold"
+			>by Diskominfo Kab Semarang</span
+		>
+	</h1>
 </nav>
 
 <!-- <div>Main</div> -->
 <main
 	class="flex min-h-screen w-full flex-wrap content-center items-center justify-center gap-8 bg-linear-to-br
- from-blue-200 via-blue-50 to-white pt-24 align-top"
+ from-blue-200 via-blue-50 to-white pt-32 pb-12 align-top"
 >
 	<!-- <p>This is a main section</p> -->
 	<!-- Instagram Diskominfo -->
 	<div id="list-item">
 		<a href="https://www.instagram.com/diskominfosemarangkab/" target="_blank"
 			><div
-				class="flex h-48 w-48 flex-col items-center justify-center rounded-2xl bg-white px-2 py-2 text-center drop-shadow-lg"
+				class="flex h-36 w-36 flex-col items-center justify-center rounded-2xl bg-white px-2 py-2 text-center drop-shadow-lg md:h-48 md:w-48"
 			>
 				<svg
 					role="img"
@@ -152,38 +156,45 @@
 
 <!-- <div>Footer</div> -->
 <footer
-	class="flex h-12 items-center justify-center rounded-t-lg bg-blue-100 py-2 text-center text-blue-950"
+	class="flex h-auto flex-col items-center justify-center rounded-t-lg bg-blue-100 py-4 text-center text-blue-950 md:flex-row md:text-base"
 >
-	&copy; {currentYear} | Build with
-	<a
-		href="https://svelte.dev/tutorial/kit/introducing-sveltekit"
-		target="_blank"
-		class="flex justify-center px-1 align-top font-bold text-orange-400"
-		><svg
-			role="img"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			width="16px"
-			class="mr-0.5 fill-current"
-			><title>Svelte</title><path
-				d="M10.354 21.125a4.44 4.44 0 0 1-4.765-1.767 4.109 4.109 0 0 1-.703-3.107 3.898 3.898 0 0 1 .134-.522l.105-.321.287.21a7.21 7.21 0 0 0 2.186 1.092l.208.063-.02.208a1.253 1.253 0 0 0 .226.83 1.337 1.337 0 0 0 1.435.533 1.231 1.231 0 0 0 .343-.15l5.59-3.562a1.164 1.164 0 0 0 .524-.778 1.242 1.242 0 0 0-.211-.937 1.338 1.338 0 0 0-1.435-.533 1.23 1.23 0 0 0-.343.15l-2.133 1.36a4.078 4.078 0 0 1-1.135.499 4.44 4.44 0 0 1-4.765-1.766 4.108 4.108 0 0 1-.702-3.108 3.855 3.855 0 0 1 1.742-2.582l5.589-3.563a4.072 4.072 0 0 1 1.135-.499 4.44 4.44 0 0 1 4.765 1.767 4.109 4.109 0 0 1 .703 3.107 3.943 3.943 0 0 1-.134.522l-.105.321-.286-.21a7.204 7.204 0 0 0-2.187-1.093l-.208-.063.02-.207a1.255 1.255 0 0 0-.226-.831 1.337 1.337 0 0 0-1.435-.532 1.231 1.231 0 0 0-.343.15L8.62 9.368a1.162 1.162 0 0 0-.524.778 1.24 1.24 0 0 0 .211.937 1.338 1.338 0 0 0 1.435.533 1.235 1.235 0 0 0 .344-.151l2.132-1.36a4.067 4.067 0 0 1 1.135-.498 4.44 4.44 0 0 1 4.765 1.766 4.108 4.108 0 0 1 .702 3.108 3.857 3.857 0 0 1-1.742 2.583l-5.589 3.562a4.072 4.072 0 0 1-1.135.499m10.358-17.95C18.484-.015 14.082-.96 10.9 1.068L5.31 4.63a6.412 6.412 0 0 0-2.896 4.295 6.753 6.753 0 0 0 .666 4.336 6.43 6.43 0 0 0-.96 2.396 6.833 6.833 0 0 0 1.168 5.167c2.229 3.19 6.63 4.135 9.812 2.108l5.59-3.562a6.41 6.41 0 0 0 2.896-4.295 6.756 6.756 0 0 0-.665-4.336 6.429 6.429 0 0 0 .958-2.396 6.831 6.831 0 0 0-1.167-5.168Z"
-			/></svg
-		>SvelteKit</a
-	>
-	and
-	<a
-		href="https://tailwindcss.com/"
-		target="_blank"
-		class="flex justify-center px-1 align-top font-bold text-cyan-500"
-		><svg
-			role="img"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			width="16px"
-			class="mr-0.5 fill-current"
-			><title>Tailwind CSS</title><path
-				d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z"
-			/></svg
-		>TailwindCSS</a
-	> | Aptika Team - Diskominfo Kab. Semarang
+	<p class="flex flex-row">&copy; {currentYear} <span class="mx-1 hidden md:block">|</span></p>
+	<div class="flex items-center justify-center">
+		<span> Build with</span>
+		<a
+			href="https://svelte.dev/tutorial/kit/introducing-sveltekit"
+			target="_blank"
+			class="flex justify-center px-1 align-top font-bold text-orange-400"
+			><svg
+				role="img"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				width="16px"
+				class="mr-0.5 fill-current"
+				><title>Svelte</title><path
+					d="M10.354 21.125a4.44 4.44 0 0 1-4.765-1.767 4.109 4.109 0 0 1-.703-3.107 3.898 3.898 0 0 1 .134-.522l.105-.321.287.21a7.21 7.21 0 0 0 2.186 1.092l.208.063-.02.208a1.253 1.253 0 0 0 .226.83 1.337 1.337 0 0 0 1.435.533 1.231 1.231 0 0 0 .343-.15l5.59-3.562a1.164 1.164 0 0 0 .524-.778 1.242 1.242 0 0 0-.211-.937 1.338 1.338 0 0 0-1.435-.533 1.23 1.23 0 0 0-.343.15l-2.133 1.36a4.078 4.078 0 0 1-1.135.499 4.44 4.44 0 0 1-4.765-1.766 4.108 4.108 0 0 1-.702-3.108 3.855 3.855 0 0 1 1.742-2.582l5.589-3.563a4.072 4.072 0 0 1 1.135-.499 4.44 4.44 0 0 1 4.765 1.767 4.109 4.109 0 0 1 .703 3.107 3.943 3.943 0 0 1-.134.522l-.105.321-.286-.21a7.204 7.204 0 0 0-2.187-1.093l-.208-.063.02-.207a1.255 1.255 0 0 0-.226-.831 1.337 1.337 0 0 0-1.435-.532 1.231 1.231 0 0 0-.343.15L8.62 9.368a1.162 1.162 0 0 0-.524.778 1.24 1.24 0 0 0 .211.937 1.338 1.338 0 0 0 1.435.533 1.235 1.235 0 0 0 .344-.151l2.132-1.36a4.067 4.067 0 0 1 1.135-.498 4.44 4.44 0 0 1 4.765 1.766 4.108 4.108 0 0 1 .702 3.108 3.857 3.857 0 0 1-1.742 2.583l-5.589 3.562a4.072 4.072 0 0 1-1.135.499m10.358-17.95C18.484-.015 14.082-.96 10.9 1.068L5.31 4.63a6.412 6.412 0 0 0-2.896 4.295 6.753 6.753 0 0 0 .666 4.336 6.43 6.43 0 0 0-.96 2.396 6.833 6.833 0 0 0 1.168 5.167c2.229 3.19 6.63 4.135 9.812 2.108l5.59-3.562a6.41 6.41 0 0 0 2.896-4.295 6.756 6.756 0 0 0-.665-4.336 6.429 6.429 0 0 0 .958-2.396 6.831 6.831 0 0 0-1.167-5.168Z"
+				/></svg
+			>SvelteKit</a
+		>
+
+		<span class="mx-1">and</span>
+		<a
+			href="https://tailwindcss.com/"
+			target="_blank"
+			class="flex justify-center px-1 font-bold text-cyan-500"
+			><svg
+				role="img"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				width="16px"
+				class="mr-0.5 fill-current"
+				><title>Tailwind CSS</title><path
+					d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z"
+				/></svg
+			>TailwindCSS</a
+		>
+	</div>
+	<p class="flex flex-row">
+		<span class="mx-1 hidden md:block">|</span>Aptika Team - Diskominfo Kab. Semarang
+	</p>
 </footer>
